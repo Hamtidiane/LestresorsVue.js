f5338d1705eb5a89449f98c7a55013e5253a7d14
Cheick  Diabate <cheick.diabate@gmail.com>
2024-09-04 12:10:04 +0200

    ajout composant CitiesList

---
+++ Documents/ProjetMeteo/src/views/CitiesList.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="CitiesList">
+    <h1>Météo-liste des villes</h1>
+  </div>
+</template>
+
+<style>
+.CitiesList h1{
+  display: flex;
+
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
